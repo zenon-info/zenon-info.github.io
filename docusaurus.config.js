d3b543143d7d7d92d16813d112665c59d41d5eaa
Zenon.info <0x3639+zenon@protonmail.com>
2022-10-23 07:48:20 -0500

    Add Schizo Category

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -124,12 +124,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/zenon-info',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Zenon.info, Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
